3e857fd8a80a92d5cf903c89cc2294219555f661
vaibhavkshtriya912-creator <vaibhavkshtroiya912@gmail.com>
2025-10-18 23:14:41 +0530

    Master_1 : to-do page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ def home():
 def api():
     data = {"name": "Asha","grade": "A"}
     return jsonify(data)
+
+@app.route("/todo")
+def todo_page():
+    return render_template("todo.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
